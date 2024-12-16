19030bfe212e135a932ad9558530e3701ca73c60
MegumiOhara <156937802+MegumiOhara@users.noreply.github.com>
2024-12-16 13:49:29 +0100

    started auth.js and changed db.js
    Followed error's suggestion.

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,4 +1,25 @@
+import express from "express";
+import bcrypt from "bcryptjs"; // For hashing passwords
+import jwt from "jsonwebtoken"; // For generating and verifying JSON Web Tokens
+import pool from "../db.js"; // Database connection pool
 
+
+const router = express.Router();
+
+//Register
+router.post("/register", async (req,res) => {
+    //extract username and password from request body
+    const { username, password } = req.body; 
+    try {
+        //Hash the password for secure storage
+        const hashedPassword = await bcrypt.hash(password, 10);
+        //Insert the user into the database
+        await pool.query("INSERT INTO users (username, password) VALUES ($1, $2)", [username, hashedPassword]);
+        res.status(201).json({ messge: "User registered successfully"});
+    } catch (error) {
+        res.status(500).json({ error: "Error registering user "}); 
+    }
+});
 
 
 export default router;
