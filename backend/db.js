d8da0fb55beac81aebb8f13dae79f271e85c6a06
MegumiOhara <156937802+MegumiOhara@users.noreply.github.com>
2024-12-17 13:10:16 +0100

    connected to database
    testing works.endpoints work

--- backend/db.js
+++ backend/db.js
@@ -7,10 +7,9 @@ dotenv.config();
 
 const pool = new Pool({
   user: process.env.DB_USER,
-  //host: process.env.DB_HOST || "localhost",
-  host: "localhost",
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASS,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE, 
+  password: process.env.DB_PASSWORD, 
   port: process.env.DB_PORT,
 });
 
