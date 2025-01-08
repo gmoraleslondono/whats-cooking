bdca8bed0df494d85e749bc1c9c4e7e5d88ca3a5
Gloria Morales <gmoraleslondono@gmail.com>
2025-01-08 16:22:08 +0100

    Swagger documentation ingredients.js and suggestions.js and improve error handling in API routes

--- backend/routes/ingredients.js
+++ backend/routes/ingredients.js
@@ -3,16 +3,50 @@ import axios from "axios";
 
 const router = Router();
 
+/**
+ * @swagger
+ * /api/ingredients?ingredient={ingredient}:
+ *   get:
+ *     summary: Get meal by ingredient
+ *     parameters:
+ *       - in: query
+ *         name: ingredient
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: Name of the ingredient
+ *     responses:
+ *       200:
+ *         description: A list of meals with the ingredient
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 meals:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       strMeal:
+ *                         type: string
+ *                       strMealThumb:
+ *                         type: string
+ *                       idMeal:
+ *                         type: string
+ *       500:
+ *         description: Error retrieving meals
+ */
 
 router.get("/", async (req, res) => {
-    const ingredient = req.query.ingredient;
-    const ingredientsAPI = `https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingredient}`;
-    try {
+  const ingredient = req.query.ingredient;
+  const ingredientsAPI = `https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingredient}`;
+  try {
     const response = await axios.get(ingredientsAPI);
-    
+
     res.json(response.data);
   } catch (error) {
-    res.json({ error: error.message });
+    res.status(500).json({ error: "Error retrieving meals." });
   }
 });
 
